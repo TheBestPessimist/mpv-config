c61409c35af5de8df7479bb27dc25656eebaac73
TheBestPessimist <cristian@tbp.land>
2021-04-09 11:47:01 +0300

    on_pause_show_osc finally works ok

--- Scripts/on_pause_show_osc.lua
+++ Scripts/on_pause_show_osc.lua
@@ -1,25 +1,32 @@
--- from https://github.com/mpv-player/mpv/issues/6592#issuecomment-488269456
+--[[
+    Show OSC on Pause
+
+Inspired from https://github.com/mpv-player/mpv/issues/6592#issuecomment-488269456
+
+
+Technical note:
+For some reason `mp.command("no-osd set osd-level 0; script-message osc-visibility always; no-osd set osd-level 1")` does not work.
+The OSC messages `OSC visibility: always/auto` are still displayed.
+It's as if `no-osd set osd-level 0` runs after `script-message osc-visibility always` is run/displayed.
+As a workaround, I have to run all the commands with an increasing delay.
+Why? No idea. Bugs? `¯\_(ツ)_/¯`
+--]]
+
+
 function on_pause_show_osc(name, value)
     if value == true then
-        mp.command("no-osd set osd-level 0; script-message osc-visibility always")
-        reset_osd()
+        delayed(0, "no-osd set osd-level 0")
+        delayed(0.1, "script-message osc-visibility always")
+        delayed(0.3, "no-osd set osd-level 1")
     else
-        mp.command("no-osd set osd-level 0; script-message osc-visibility auto")
-        reset_osd()
+        delayed(0, "no-osd set osd-level 0")
+        delayed(0.1, "script-message osc-visibility auto")
+        delayed(0.3, "no-osd set osd-level 1")
     end
 end
 
--- For some reason `set osd-level 0; script-message osc-visibility always; no-osd set osd-level 1` does not work.
--- The OSC messages `OSC visibility: always/auto` are still displayed.
--- It's as if `no-osd set osd-level 1` runs before `script-message osc-visibility always` is run/displayed.`¯\_(ツ)_/¯`
--- As a workaround, I have to make the osd visible after a 1 second timeout.
--- Why? No idea. Bugs?`¯\_(ツ)_/¯`
-function reset_osd()
-    mp.add_timeout(2,
-        function()
-            mp.command("no-osd set osd-level 1")
-        end
-    )
+function delayed(time, command)
+   mp.add_timeout(time, function() mp.command(command) end)
 end
 
 mp.observe_property("pause", "bool", on_pause_show_osc)
