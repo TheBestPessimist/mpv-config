1cddbe09164c54276fed67beabe5845199cb3232
TheBestPessimist <cristian+gitpush@tbp.land>
2024-11-06 11:35:41 +0200

    simplify on_pause_show_osc

--- Scripts/on_pause_show_osc.lua
+++ Scripts/on_pause_show_osc.lua
@@ -1,32 +1,17 @@
 --[[
     Show OSC on Pause
 
-Inspired from https://github.com/mpv-player/mpv/issues/6592#issuecomment-488269456
+Inspired from https://github.com/mpv-player/mpv/issues/6592
+]]
 
-
-Technical note:
-For some reason `mp.command("no-osd set osd-level 0; script-message osc-visibility always; no-osd set osd-level 1")` does not work.
-The OSC messages `OSC visibility: always/auto` are still displayed.
-It's as if `no-osd set osd-level 0` runs after `script-message osc-visibility always` is run/displayed.
-As a workaround, I have to run all the commands with an increasing delay.
-Why? No idea. Bugs? `¯\_(ツ)_/¯`
---]]
 
 
 function on_pause_show_osc(name, value)
-    if value == true then
-        delayed(0, "no-osd set osd-level 0")
-        delayed(0.1, "script-message osc-visibility always")
-        delayed(0.3, "no-osd set osd-level 1")
-    else
-        delayed(0, "no-osd set osd-level 0")
-        delayed(0.1, "script-message osc-visibility auto")
-        delayed(0.3, "no-osd set osd-level 1")
-    end
-end
-
-function delayed(time, command)
-   mp.add_timeout(time, function() mp.command(command) end)
+	if value == true then
+		mp.commandv('script-message','osc-visibility','always','no-osd')
+	else
+		mp.commandv('script-message','osc-visibility','auto','no-osd')
+	end
 end
 
 mp.observe_property("pause", "bool", on_pause_show_osc)
