6999ac61e496902e77fc50cd57a782b0312ebc68
TheBestPessimist <cristian@tbp.land>
2025-10-05 10:51:42 +0300

    show progress during seek instead of 'osc'

--- Scripts/show-osc-during-seek.lua
+++ Scripts/show-osc-during-seek.lua
@@ -1,11 +1,11 @@
-/*
+--[[
     For this to work, you must also add the following config in `mpv.conf`:
 
     osd-on-seek=no
-*/
-
-mp.observe_property('seeking', 'native', function(_, seeking)
-    if seeking then
-        mp.command('script-message osc-show')
+ ]]
+mp.observe_property('seeking', 'native', function(name, isSeeking)
+    if isSeeking then
+--         mp.command('script-message osc-show')
+        mp.command('show-progress')
     end
 end)
